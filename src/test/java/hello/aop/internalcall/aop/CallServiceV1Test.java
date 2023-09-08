08d7cfb30d7f39f83490506cc01964f4a932bfba
YeongBB <handfaste@naver.com>
2023-09-08 18:25:06 +0900

    infusion of oneself

---
+++ src/test/java/hello/aop/internalcall/aop/CallServiceV1Test.java
@@ -0,0 +1,28 @@
+package hello.aop.internalcall.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Slf4j
+@SpringBootTest
+@Import(CallLogAspect.class)
+class CallServiceV1Test {
+
+    @Autowired
+    CallServiceV1 callServiceV1;
+
+    @Test
+    void external() {
+        callServiceV1.external();
+    }
+
+    @Test
+    void internal() {
+        callServiceV1.internal();
+    }
+}
